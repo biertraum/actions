f4b3d16c2cb336a6b58b827cf3d813dc8ca3876a
Pavel Livinskiy <plivinskiy@tagwork-one.de>
2023-12-01 19:00:38 +0100

    fix(devops): disable weltpixel css generation

--- config/pipelines/scripts/staging/release_setup.sh
+++ config/pipelines/scripts/staging/release_setup.sh
@@ -16,19 +16,19 @@ php bin/magento setup:static-content:deploy de_DE -f -j 3
 php bin/magento setup:static-content:deploy en_US --area adminhtml -f -j 3
 
 # generate CSS for the Theme weltpixel
-php bin/magento weltpixel:less:generate
-php bin/magento weltpixel:css:generate --store default
-php bin/magento weltpixel:css:generate --store bw_de
-php bin/magento weltpixel:css:generate --store bb_de
-php bin/magento weltpixel:css:generate --store hp_de
-php bin/magento weltpixel:css:generate --store riegele_de
-php bin/magento weltpixel:css:generate --store schoenramer_de
-php bin/magento weltpixel:css:generate --store wittmann_de
-php bin/magento weltpixel:css:generate --store floetzinger_de
-php bin/magento weltpixel:css:generate --store biertraum_de
-php bin/magento weltpixel:css:generate --store alpenbrauerei_de
-php bin/magento weltpixel:css:generate --store alpenbrauerei_alpenstoff_de
-php bin/magento weltpixel:css:generate --store alpenbrauerei_buergerbraeu_de
-php bin/magento weltpixel:css:generate --store bf_de
-php bin/magento weltpixel:css:generate --store hirsch_de
-php bin/magento weltpixel:css:generate --store meckatzer_de
+#php bin/magento weltpixel:less:generate
+#php bin/magento weltpixel:css:generate --store default
+#php bin/magento weltpixel:css:generate --store bw_de
+#php bin/magento weltpixel:css:generate --store bb_de
+#php bin/magento weltpixel:css:generate --store hp_de
+#php bin/magento weltpixel:css:generate --store riegele_de
+#php bin/magento weltpixel:css:generate --store schoenramer_de
+#php bin/magento weltpixel:css:generate --store wittmann_de
+#php bin/magento weltpixel:css:generate --store floetzinger_de
+#php bin/magento weltpixel:css:generate --store biertraum_de
+#php bin/magento weltpixel:css:generate --store alpenbrauerei_de
+#php bin/magento weltpixel:css:generate --store alpenbrauerei_alpenstoff_de
+#php bin/magento weltpixel:css:generate --store alpenbrauerei_buergerbraeu_de
+#php bin/magento weltpixel:css:generate --store bf_de
+#php bin/magento weltpixel:css:generate --store hirsch_de
+#php bin/magento weltpixel:css:generate --store meckatzer_de
