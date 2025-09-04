39aa9a218bc9689a49c8abc23a1787c4889b7c4f
Pavel Livinskiy <pavel.livinskiy@port-neo.com>
2025-09-04 22:58:22 +0200

    fix(deploy): update deploy scripts to use default PHP binary instead of hardcoded PHP 8.1 paths

--- config/pipelines/scripts/staging/post_release_setup.sh
+++ config/pipelines/scripts/staging/post_release_setup.sh
@@ -9,19 +9,19 @@ then
 fi
 
 echo "Import magento config"
-/usr/bin/php8.1 bin/magento app:config:import --no-interaction
+/usr/bin/php bin/magento app:config:import --no-interaction
 
 echo "Check setup:upgrade status"
 # use --no-ansi to avoid color characters
-message=$(/usr/bin/php8.1 bin/magento setup:db:status --no-ansi)
+message=$(/usr/bin/php bin/magento setup:db:status --no-ansi)
 
 if [[ ${message:0:3} == "All" ]]; then
   echo "No setup upgrade - clear cache";
-  /usr/bin/php8.1 bin/magento cache:clean
+  /usr/bin/php bin/magento cache:clean
 else
   echo "Run setup:upgrade - maintenance mode"
-  /usr/bin/php8.1 bin/magento maintenance:enable
-  /usr/bin/php8.1 bin/magento setup:upgrade --keep-generated --no-interaction
-  /usr/bin/php8.1 bin/magento maintenance:disable
-  /usr/bin/php8.1 bin/magento cache:flush
+  /usr/bin/php bin/magento maintenance:enable
+  /usr/bin/php bin/magento setup:upgrade --keep-generated --no-interaction
+  /usr/bin/php bin/magento maintenance:disable
+  /usr/bin/php bin/magento cache:flush
 fi
--- config/pipelines/scripts/staging/release_setup.sh
+++ config/pipelines/scripts/staging/release_setup.sh
@@ -9,26 +9,26 @@ fi
 
 # operation to make before the switch of the release folder
 # Generate code
-/usr/bin/php8.1 bin/magento setup:di:compile
+/usr/bin/php bin/magento setup:di:compile
 
 # Build static files
-/usr/bin/php8.1 bin/magento setup:static-content:deploy de_DE -f -j 3
-/usr/bin/php8.1 bin/magento setup:static-content:deploy en_US --area adminhtml -f -j 3
+/usr/bin/php bin/magento setup:static-content:deploy de_DE -f -j 3
+/usr/bin/php bin/magento setup:static-content:deploy en_US --area adminhtml -f -j 3
 
 # generate CSS for the Theme weltpixel
-/usr/bin/php8.1 bin/magento weltpixel:less:generate
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store default
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store bw_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store bb_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store hp_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store riegele_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store schoenramer_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store wittmann_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store floetzinger_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store biertraum_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store alpenbrauerei_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store alpenbrauerei_alpenstoff_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store alpenbrauerei_buergerbraeu_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store bf_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store hirsch_de
-/usr/bin/php8.1 bin/magento weltpixel:css:generate --store meckatzer_de
+/usr/bin/php bin/magento weltpixel:less:generate
+/usr/bin/php bin/magento weltpixel:css:generate --store default
+/usr/bin/php bin/magento weltpixel:css:generate --store bw_de
+/usr/bin/php bin/magento weltpixel:css:generate --store bb_de
+/usr/bin/php bin/magento weltpixel:css:generate --store hp_de
+/usr/bin/php bin/magento weltpixel:css:generate --store riegele_de
+/usr/bin/php bin/magento weltpixel:css:generate --store schoenramer_de
+/usr/bin/php bin/magento weltpixel:css:generate --store wittmann_de
+/usr/bin/php bin/magento weltpixel:css:generate --store floetzinger_de
+/usr/bin/php bin/magento weltpixel:css:generate --store biertraum_de
+/usr/bin/php bin/magento weltpixel:css:generate --store alpenbrauerei_de
+/usr/bin/php bin/magento weltpixel:css:generate --store alpenbrauerei_alpenstoff_de
+/usr/bin/php bin/magento weltpixel:css:generate --store alpenbrauerei_buergerbraeu_de
+/usr/bin/php bin/magento weltpixel:css:generate --store bf_de
+/usr/bin/php bin/magento weltpixel:css:generate --store hirsch_de
+/usr/bin/php bin/magento weltpixel:css:generate --store meckatzer_de
